fe141ad3ddc19d42dd210a4a3e4c02e570771f49
Elia <eliatoma02@gmail.com>
2024-01-13 22:54:26 +0100

    first commit

---
+++ src/main/java/it/unicam/cs/ids/localplatform/IdsProject2324Application.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.localplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
+public class IdsProject2324Application {
+
+	public static void main(String[] args) {
+		//SpringApplication.run(IdsProject2324Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/unicam/cs/ids/localplatform/IdsProject2324ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.localplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class IdsProject2324ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
