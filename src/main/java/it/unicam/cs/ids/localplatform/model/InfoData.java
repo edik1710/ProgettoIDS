e6450ef04fc4ac07710bad13ae6f8646fab1243d
Elia <eliatoma02@gmail.com>
2024-01-17 11:14:32 +0100

    Deleted useless fields

--- src/main/java/it/unicam/cs/ids/localplatform/model/InfoData.java
+++ src/main/java/it/unicam/cs/ids/localplatform/model/InfoData.java
@@ -1,8 +1,6 @@
 package it.unicam.cs.ids.localplatform.model;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 public class InfoData {
     private Date publicationDate;
@@ -11,14 +9,12 @@ public class InfoData {
     Il tipo è da definire
      */
     private User author;
-    private List<Date> changeHistory;
     private boolean pending;
     private boolean awaitingVerification;
 
     public InfoData(Date publicationDate, User author, boolean pending) {
         this.publicationDate = publicationDate;
         this.author = author;
-        this.changeHistory = new ArrayList<>(); // Quando viene creato un InfoData, la sua changeHistory è vuota
         this.pending = pending;
         this.awaitingVerification = false; // Quando viene creato un InfoData, non è in attesa di verifica
     }
@@ -29,10 +25,6 @@ public class InfoData {
 
     public User getAuthor() {
         return author;
-    }
-
-    public List<Date> getChangeHistory() {
-        return changeHistory;
     }
 
     public boolean isPending() {
