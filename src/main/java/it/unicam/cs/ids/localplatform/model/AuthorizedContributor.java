71e5225f1166031fc393ab2d6fd8b45f1d03558f
Elia <eliatoma02@gmail.com>
2024-01-20 23:18:04 +0100

    Implemented AuthorizedContributor

--- src/main/java/it/unicam/cs/ids/localplatform/model/AuthorizedContributor.java
+++ src/main/java/it/unicam/cs/ids/localplatform/model/AuthorizedContributor.java
@@ -1,6 +1,10 @@
 package it.unicam.cs.ids.localplatform.model;
 
 import it.unicam.cs.ids.localplatform.MunicipalTerritory;
+import it.unicam.cs.ids.localplatform.command.*;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * This class represents an authorized contributor.<br><br>
@@ -11,5 +15,96 @@ public class AuthorizedContributor extends User {
 
     public AuthorizedContributor(String name, String surname, String email, String password, MunicipalTerritory residence, String cf) {
         super(name, surname, email, password, residence, cf);
+    }
+
+    // TODO: aggiungere un controllo per oggetti già presenti nel sistema
+
+    /**
+     * This method allows an authorized contributor to publish a new point of interest to the platform.
+     *
+     * @param title       The title of the point of interest.
+     * @param coordinates The coordinates of the point of interest.
+     */
+    public void publishPOI(String title, Coordinates coordinates) {
+        new CreatePOICommand(title, new Date(), this, coordinates, this.getResidence()).execute();
+    }
+
+    /**
+     * This method allows an authorized contributor to publish a new general content to the platform.
+     *
+     * @param content The content to be published.
+     */
+    public void publishGeneralContent(Content content) {
+        new CreateGeneralContentCommand(content, this.getResidence()).execute();
+    }
+
+    /**
+     * This method allows an authorized contributor to publish a new itinerary to the platform.
+     *
+     * @param title The title of the itinerary.
+     * @param POIs  The list of points of interest that make up the itinerary.
+     */
+    public void publishItinerary(String title, List<POI> POIs) {
+        new CreateItineraryCommand(title, new Date(), this, POIs, this.getResidence()).execute();
+    }
+
+    /**
+     * This method allows an authorized contributor to publish a new content to a point of interest.
+     *
+     * @param poi     The point of interest to which the content is to be associated.
+     * @param content The content to be published.
+     */
+    public void publishPOIContent(POI poi, Content content) {
+        new CreatePOIContentCommand(poi, content).execute();
+    }
+
+    /**
+     * This method allows an authorized contributor to publish a change to a point of interest.
+     *
+     * @param poi   The point of interest to be changed.
+     * @param title The new title of the point of interest.
+     */
+    public void publishChangesToPOI(POI poi, String title) {
+        new ChangePOICommand(poi, title).execute();
+    }
+
+    // Il metodo submitChangesToGeneralContent() non è stato implementato in quanto non è stato definito un modo per identificare univocamente un contenuto generico.
+    // Dopo l'implementazione inserire i parametri nel file vpp
+    public void publishChangesToExistingContent() {
+        // TODO
+    }
+
+    /**
+     * This method allows an authorized contributor to publish a change to an itinerary.
+     *
+     * @param itinerary The itinerary to be changed.
+     * @param title     The new title of the itinerary.
+     */
+    public void publishChangesToItinerary(Itinerary itinerary, String title) {
+        new ChangeItineraryCommand(itinerary, title).execute();
+    }
+
+    /**
+     * This method allows an authorized contributor to delete a point of interest from the platform.
+     *
+     * @param poi The point of interest to be deleted.
+     */
+    public void deletePOI(POI poi) {
+        new DeletePOICommand(this.getResidence(), poi.getCoordinates()).execute();
+    }
+
+    // Il metodo submitChangesToGeneralContent() non è stato implementato in quanto non è stato definito un modo per identificare univocamente un contenuto generico.
+    // Dopo l'implementazione inserire i parametri nel file vpp
+    public void deleteContent() {
+        // TODO
+    }
+
+    /**
+     * This method allows an authorized contributor to delete an itinerary from the platform.
+     *
+     * @param itinerary The itinerary to be deleted.
+     */
+    public void deleteItinerary(Itinerary itinerary) {
+        new DeleteItineraryCommand(this.getResidence(), itinerary).execute();
     }
 }
