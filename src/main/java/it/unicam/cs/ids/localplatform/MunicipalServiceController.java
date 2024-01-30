b153240d28d7135e488786518f90f682c2477b71
Elia <eliatoma02@gmail.com>
2024-01-30 19:53:36 +0100

    Implemented POI creation from web interface

--- src/main/java/it/unicam/cs/ids/localplatform/MunicipalServiceController.java
+++ src/main/java/it/unicam/cs/ids/localplatform/MunicipalServiceController.java
@@ -23,32 +23,24 @@ public class MunicipalServiceController {
         this.currentUser = currentUser;
     }
 
-    private User currentUser ;
+    private User currentUser;
 
     public User getCurrentUser() {
         return currentUser;
     }
 
-    @GetMapping("/Login")
+    @PostMapping("/Login")
     public ResponseEntity<Object> getLoginInfo(@RequestBody String[] array) {
-        /*
-        credentialListRepository.save(array[0]);
-        credentialListRepository.save(array[1]);
-        credentialListRepository.save(array[2]);
-        credentialListRepository.save(array[3]);
-        credentialListRepository.save(array[4]);
-        credentialListRepository.save(array[5]);
-        */
-
         MunicipalTerritory mt = new MunicipalTerritory(array[4]);
         this.currentUser = new User(array[0], array[1], array[2], array[3], mt, array[5]);
 
-        System.out.println("Mannaggia a quelli della prima fila" + currentUser);
+        //System.out.println("Mannaggia a quelli della prima fila" + currentUser);
         return new ResponseEntity<>("User saved", HttpStatus.OK);
     }
 
     @RequestMapping("/POIs")
     public ResponseEntity<Object> getPOIs() {
+        /*
         Coordinates coordinates = new Coordinates(43.133333, 13.066667);
         Contributor contributor = new Contributor("Mario", "Rossi", "email", "password", municipalTerritory, "codiceFiscale");
         POI p1 = new POI("piazza del comune", new Date(), contributor, coordinates, "Piazza del Comune di Camerino");
@@ -57,7 +49,7 @@ public class MunicipalServiceController {
         Coordinates coordinates1 = new Coordinates(52, 75);
         POI p2 = new POI("piazza del comune", new Date(), contributor, coordinates1, "Piazza del Comune di Camerino");
         this.municipalTerritory.getPOIs().put(coordinates1, p2);
-
+        */
 
         // Recupera la mappa dei POI da MunicipalTerritory
         Map<Coordinates, POI> poiMap = municipalTerritory.getPOIs();
@@ -69,7 +61,7 @@ public class MunicipalServiceController {
             poiListStringArray[poiList.indexOf(poi)] = poi.toString();
         }
 
-        System.out.println(currentUser);
+        //System.out.println(currentUser);
 
         // Restituisce la lista di POI
         return new ResponseEntity<>(poiListStringArray, HttpStatus.OK);
@@ -131,14 +123,16 @@ public class MunicipalServiceController {
         return new ResponseEntity<>(pois.get(name), HttpStatus.OK);
     }
 
+    // FUNZIONA MANNAGGIA A CHI PRENDE IL COVID ALLE CRESIME
     @PostMapping("/AggiungiPOI")
     public ResponseEntity<String> addPOI(@RequestBody String[] array) {
 
-        Contributor contributor = new Contributor("Eracleonte", "Villa", "email", "password", municipalTerritory, "codiceFiscale");
-        Coordinates coordinates = new Coordinates(43, 13);
+        //Contributor contributor = new Contributor("Eracleonte", "Villa", "email", "password", municipalTerritory, "codiceFiscale");
+        //Coordinates c = new Coordinates(43, 13);
 
-        // contributor sarà sostituito da currentUser
-        POI POI = new POI(array[0], new Date(), contributor, coordinates, array[1]);
+        Coordinates coordinates = new Coordinates(Double.parseDouble(array[2]), Double.parseDouble(array[3]));
+
+        POI POI = new POI(array[0], new Date(), currentUser, coordinates, array[1]);
 
         if (!this.municipalTerritory.getPOIs().containsKey(POI.getCoordinates())) {
             this.municipalTerritory.addPOI(POI);
@@ -146,15 +140,6 @@ public class MunicipalServiceController {
         } else {
             return new ResponseEntity<>("Punto d'Interesse già Esistente", HttpStatus.BAD_REQUEST);
         }
-
-        /*
-        if (!pois.containsKey(POI.getTitle())) {
-            pois.put(POI.getTitle(), POI);
-            return new ResponseEntity<>("Punto d'Interesse aggiunto correttamente", HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>("Punto d'Interesse già Esistente", HttpStatus.BAD_REQUEST);
-        }
-        */
     }
 
     @DeleteMapping("/POI/{name}")
